67e7486bafcd386fe6d1990e447782cd6cf16935
PRASHANT KUMAR SAH <zzpscode@gmail.com>
2026-09-07 11:30:54 +0530

    Add Windows build script

---
+++ build_windows.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m PyInstaller --noconfirm --clean --onefile --windowed --name PhotoSignatureStudio app.py
+if errorlevel 1 exit /b 1
+echo BUILD COMPLETE: dist\PhotoSignatureStudio.exe
